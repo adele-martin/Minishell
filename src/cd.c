ff3e4ab9b39d55ebbea6e555acbe1ebd475086d9
moldypeaches <adelejulietmartin@gmail.com>
2024-09-11 14:52:39 +0200

    ok

--- src/cd.c
+++ src/cd.c
@@ -38,7 +38,7 @@ int	builtin_cd (t_data *data)
 	char *current_dir;
 	char *previous_pwd;
 
-	if (data->cmd_argc > 2)
+	if (data->cmd_argc >= 2)
 	{
 		ft_printf("minishell: cd: too many arguments\n");
 		return (1);
