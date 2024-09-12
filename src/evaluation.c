4ed68fd77f6f05a6b780311712bb7fefd86c14b9
bschneid <bschneid@student.42berlin.de>
2024-09-12 16:40:15 +0200

    FIXES EXECUTE

--- src/evaluation.c
+++ src/evaluation.c
@@ -6,33 +6,18 @@
 /*   By: bschneid <bschneid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 18:11:03 by bschneid          #+#    #+#             */
-/*   Updated: 2024/09/12 15:16:41 by bschneid         ###   ########.fr       */
+/*   Updated: 2024/09/12 16:39:59 by bschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
 static int	get_argc(char **argv);
+static char	run_builtin(t_data *data);
+static int	run_extern(t_data *data);
+int			action_in_child(t_data *data);
 static int	run_from_bin_path(t_data *data);
 char		**create_argv(t_list *linked_args);
-
-/*
-The builtins should not be executed in the child process, but in the parent process:
-◦ echo with option -n
-◦ cd with only a relative or absolute path
-◦ pwd with no options
-◦ export with no options
-◦ unset with no options
-◦ env with no options or arguments
-◦ exit with no options
-void builtin_export(char **argv, int argc, char **list_envs);
-void	builtin_echo(char **argv, int argc);
-void builtin_env(char **argv, int argc, char **list_envs);
-void builtin_unset(char **argv, char **list_envs);
-void builtin_cd(char **argv, int argc);
-void builtin_pwd(char **argv, int argc);
-void builtin_exit(char **argv, int argc);
-*/
 
 // just for debugging
 // static void	print_argv(char **argv)
@@ -44,6 +29,7 @@ void builtin_exit(char **argv, int argc);
 // 		ft_printf("ARG: -->%s<--\n", argv[i++]);
 // }
 
+// remove list elements containing empty/no strings
 void	clean_args(t_list **args)
 {
 	t_list	*tmp;
@@ -90,19 +76,35 @@ int	execute(char *input, t_data *data)
 		ft_lstclear(&data->linked_args, free);
 		return (error_message(NULL, NULL, "Error in expanding variables"), 1);
 	}
-
 	clean_args(&data->linked_args); // remove empty strings
 	if (!data->linked_args)
 		return (0);
-
 	data->cmd_argv = create_argv(data->linked_args);
 	if (!data->cmd_argv)
 		return (1);
-
-	// print_argv(data->cmd_argv);
-	
 	data->cmd_argc = get_argc(data->cmd_argv);
-	// possibilities for executions:
+	if (!run_builtin(data))
+		data->status = run_extern(data);
+	else if (!data->in_child)	// if builtin was run in child, don't wait for it
+		return (data->status);
+	waitpid(data->id, &data->status, 0);
+	if (WIFEXITED(data->status))
+		data->status = WEXITSTATUS(data->status);
+	return (data->status);
+}
+
+static int	get_argc(char **argv)
+{
+	int	argc;
+
+	argc = 0;
+	while (*(argv++))
+		argc++;
+	return (argc);
+}
+
+static char	run_builtin(t_data *data)
+{
 	if (ft_strncmp(*data->cmd_argv, "echo", 5) == 0)
 		data->status = builtin_echo(data->cmd_argv, data->cmd_argc);
 	else if (ft_strncmp(*data->cmd_argv, "cd", 3) == 0)
@@ -118,51 +120,42 @@ int	execute(char *input, t_data *data)
 	else if (ft_strncmp(*data->cmd_argv, "exit", 5) == 0)
 		data->status = builtin_exit(data->cmd_argv, data->cmd_argc, data);
 	else
-	{
-		if (!data->in_child)
-			data->id = fork();
-		if (data->id == -1)
-			return (1);
-		if (data->in_child || data->id == 0)
-		{
-			if (**data->cmd_argv == '.' || **data->cmd_argv == '/' || **data->cmd_argv == '~')
-			{
-				if (ft_strncmp(*data->cmd_argv, "~/", 2) == 0)
-					update_home(data, data->cmd_argv);
-				if (access(data->cmd_argv[0], X_OK) == 0)
-					exit(ft_free(data, execve(data->cmd_argv[0], data->cmd_argv, data->list_envs)));
-				error_message(data->cmd_argv[0], NULL, strerror(errno));
-				exit (ft_free(data, 126));
-			}
-			else
-				exit(ft_free(data, run_from_bin_path(data)));
-		}
-		// waitpid(data->id, &data->status, 0);
-		// if (WIFEXITED(data->status))
-		// 	data->status = WEXITSTATUS(data->status);
-	}
-	if (!data->in_child)
-	// TODO: Care if a builtin was run and handle wait differently?!
-	{
-		waitpid(data->id, &data->status, 0);
-		// if (WIFSIGNALED(data->status))
-		// 	data->status = 128 + WTERMSIG(data->status);
-		if (WIFEXITED(data->status))
-			data->status = WEXITSTATUS(data->status);
-		// ft_printf("exit status: %d\n", data->status);
-		return (data->status);
-	}
-	exit (ft_free(data, data->status));
+		return (0);
+	return (1);
 }
 
-static int	get_argc(char **argv)
+static int	run_extern(t_data *data)
 {
-	int	argc;
+	if (!data->in_child)
+		data->id = fork();
+	if (data->id == -1)
+		return (1);
+	if (data->in_child || data->id == 0)
+	{
+		handle_signals(3);
+		exit (ft_free(data, action_in_child(data)));
+	}
+	waitpid(data->id, &data->status, 0);
+	if (WIFEXITED(data->status))
+		data->status = WEXITSTATUS(data->status);
+	return (data->status);
+}
 
-	argc = 0;
-	while (*(argv++))
-		argc++;
-	return (argc);
+int	action_in_child(t_data *data)
+{
+	if (**data->cmd_argv == '.' || **data->cmd_argv == '/' || **data->cmd_argv == '~')
+	{
+		if (ft_strncmp(*data->cmd_argv, "~/", 2) == 0)
+			update_home(data, data->cmd_argv);
+		if (access(data->cmd_argv[0], X_OK) == 0)
+			exit(ft_free(data, execve(data->cmd_argv[0], data->cmd_argv, data->list_envs)));
+		error_message(data->cmd_argv[0], NULL, strerror(errno));
+
+		exit (ft_free(data, 127));	// meaning no such file or directory
+	}
+	else
+		exit(ft_free(data, run_from_bin_path(data)));
+	return (0);
 }
 
 void	update_home(t_data *data, char **argv)
