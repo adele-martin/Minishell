e9487bf41ef6c21a0f45027ea6257ad63f899a27
ademarti <ademarti@c4b5c4.42berlin.de>
2024-09-16 09:33:42 +0200

    norm done

--- src/var_init.c
+++ src/var_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   var_init.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschneid <bschneid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 20:43:06 by bschneid          #+#    #+#             */
-/*   Updated: 2024/09/13 20:43:15 by bschneid         ###   ########.fr       */
+/*   Updated: 2024/09/16 09:33:15 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	**envs_list(char **envp)
 	{
 		list_envs[i] = ft_strdup(envp[i]);
 		if (!list_envs[i])
-			return NULL;
+			return (NULL);
 		i++;
 	}
 	while (i < 400)
@@ -35,52 +35,4 @@ char	**envs_list(char **envp)
 		i++;
 	}
 	return (list_envs);
-}
-
-//TODO: protect malloc in this function
-int expand_list(char **argv, t_list *head)
-{
-	t_list	*temp;
-	char	*str;
-	int		len;
-	int		found;
-
-	temp = head;
-	while (temp != NULL)
-	{
-		str = (char *)temp->content;
-		len = 0;
-		while (argv[1][len] != '=')
-			len++;
-		if (ft_strncmp(str, argv[1], len) == 0)
-		{
-			found = 1;
-			free(temp->content);
-			temp->content = ft_strdup(argv[1]);
-			//if (!temp->content)
-			//	return 0;
-		}
-		temp = temp->next;
-	}
-	if (!found)
-	{
-		t_list *new_node = malloc(sizeof(t_list));
-		if (!new_node)
-			return (0);
-		new_node->content = ft_strdup(argv[1]);
-		if (!new_node->content)
-			return (0);
-		new_node->next = NULL;
-		if (head == NULL) {
-			head = new_node;
-		} else {
-			temp = head;
-			while (temp->next != NULL)
-				temp = temp->next;
-			temp->next = new_node;
-		}
-		return (1);
-	}
-	else
-		return (0);
 }
